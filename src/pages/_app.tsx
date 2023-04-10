8529584c974441b91c6eac7ef58c54a304278dc0
Lucas Scommegna <scommegnal@hotmail.com>
2023-04-10 14:18:18 -0300

    miragejs config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,12 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { SidebarDrawerProvider } from "../contexts/SideBarDrawerContext";
+import { makeServer } from "../services/mirage";
+
+// Runs fake backend API if env is for development
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
