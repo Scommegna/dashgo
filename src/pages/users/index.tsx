d6dc8323990182165cdec534748e79150a62e395
Lucas Scommegna <scommegnal@hotmail.com>
2023-04-12 15:02:20 -0300

    fake users data for miragejs

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -20,12 +20,20 @@ import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 import Link from "next/link";
 
+import { useEffect } from "react";
+
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     md: true,
     lg: true,
   });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <Box>
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import { Model, createServer } from "miragejs";
+import { Factory, Model, createServer } from "miragejs";
+import { faker } from "@faker-js/faker";
 
 // User type
 type User = {
@@ -13,6 +14,24 @@ export function makeServer() {
     models: {
       user: Model.extend<Partial<User>>({}),
     },
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`;
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
+
+    seeds(server) {
+      server.createList("user", 200);
+    },
+
     routes() {
       this.namespace = "api";
       this.timing = 750;
