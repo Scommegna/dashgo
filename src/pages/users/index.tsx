7ab18b03e53af0c6e5c54abf1df602a9aa61b8a5
Lucas Scommegna <scommegnal@hotmail.com>
2023-04-18 09:04:57 -0300

    user page spinner to sinalize refetching

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -32,7 +32,7 @@ interface User {
 
 export default function UserList() {
   // Query to store data in cache and updates it
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
       const response = await fetch("http://localhost:3000/api/users");
@@ -76,6 +76,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
